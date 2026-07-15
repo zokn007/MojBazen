e518b7e9830d02168a349d65d1e9073bcf4482aa
zokn007 <gz4sbmypky@privaterelay.appleid.com>
2026-07-15 07:48:02 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='moj-bazen-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
